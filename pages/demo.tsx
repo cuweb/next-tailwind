96e4a16d6996ecb45780b4d1ea5c25fb61512672
Troy Chaplin <troychaplin@Troys-MacBook-Pro.local>
2022-09-13 18:20:49 -0400

    Updated nav styles

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -1,25 +1,25 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Featured from '../components/Featured/Featured';
-import Footer from '../components/Footer/Footer';
-import Header from '../components/Header/Header';
-import Navbar from '../components/Navbar/Navbar';
-import Spotlight from '../components/Spotlight/Spotlight';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Featured from "../components/Featured/Featured";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import Navbar from "../components/Navbar/Navbar";
+import Spotlight from "../components/Spotlight/Spotlight";
 
 const Demo: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Carleton University | Home </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
-      <Header />
-      <Spotlight />
-      <Featured />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Carleton University | Home </title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Navbar />
+			{/* <Header /> */}
+			{/* <Spotlight /> */}
+			{/* <Featured /> */}
+			{/* <Footer /> */}
+		</>
+	);
 };
 
 export default Demo;
