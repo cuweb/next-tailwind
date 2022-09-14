d203c4b660f4012ac627e80c6c5a9a1fbe625ed7
Michael Corkum <mcorkum@gmail.com>
2022-09-14 11:27:47 -0400

    Added Toggles / Validation example

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -14,12 +14,16 @@
   }
   ```
 */
+import { useState } from 'react';
+import { Switch } from '@headlessui/react';
+
 import {
   CreditCardIcon,
   KeyIcon,
   SquaresPlusIcon,
   UserCircleIcon,
   UserGroupIcon,
+  ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 
 const navigation = [
@@ -35,6 +39,10 @@ function classNames(...classes: any) {
 }
 
 export default function Form() {
+  const [availableToHire, setAvailableToHire] = useState(true);
+  const [privateAccount, setPrivateAccount] = useState(false);
+  const [allowCommenting, setAllowCommenting] = useState(true);
+  const [allowMentions, setAllowMentions] = useState(true);
   return (
     <div>
       <form action="#" method="POST">
@@ -68,6 +76,79 @@ export default function Form() {
                     Change
                   </button>
                 </div>
+              </div>
+
+              <div className="col-span-3">
+                <ul role="list" className="mt-2 divide-y divide-gray-200">
+                  <Switch.Group
+                    as="li"
+                    className="flex items-center justify-between py-4"
+                  >
+                    <div className="flex flex-col">
+                      <Switch.Label
+                        as="p"
+                        className="text-sm font-medium text-gray-900"
+                        passive
+                      >
+                        Use Preferred First Name
+                      </Switch.Label>
+                      <Switch.Description className="text-sm text-gray-500">
+                        Nulla amet tempus sit accumsan. Aliquet turpis sed sit
+                        lacinia.
+                      </Switch.Description>
+                    </div>
+                    <Switch
+                      checked={availableToHire}
+                      onChange={setAvailableToHire}
+                      className={classNames(
+                        availableToHire ? 'bg-cu-red' : 'bg-gray-200',
+                        'relative ml-4 inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2'
+                      )}
+                    >
+                      <span
+                        aria-hidden="true"
+                        className={classNames(
+                          availableToHire ? 'translate-x-5' : 'translate-x-0',
+                          'inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+                        )}
+                      />
+                    </Switch>
+                  </Switch.Group>
+                  <Switch.Group
+                    as="li"
+                    className="flex items-center justify-between py-4"
+                  >
+                    <div className="flex flex-col">
+                      <Switch.Label
+                        as="p"
+                        className="text-sm font-medium text-gray-900"
+                        passive
+                      >
+                        Make account private
+                      </Switch.Label>
+                      <Switch.Description className="text-sm text-gray-500">
+                        Pharetra morbi dui mi mattis tellus sollicitudin cursus
+                        pharetra.
+                      </Switch.Description>
+                    </div>
+                    <Switch
+                      checked={privateAccount}
+                      onChange={setPrivateAccount}
+                      className={classNames(
+                        privateAccount ? 'bg-teal-500' : 'bg-gray-200',
+                        'relative ml-4 inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2'
+                      )}
+                    >
+                      <span
+                        aria-hidden="true"
+                        className={classNames(
+                          privateAccount ? 'translate-x-5' : 'translate-x-0',
+                          'inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+                        )}
+                      />
+                    </Switch>
+                  </Switch.Group>
+                </ul>
               </div>
 
               <div className="col-span-3">
@@ -171,18 +252,32 @@ export default function Form() {
 
               <div className="col-span-6 sm:col-span-4">
                 <label
-                  htmlFor="email-address"
+                  htmlFor="email"
                   className="block text-sm font-medium text-gray-700"
                 >
-                  Email address
+                  E-mail Address
                 </label>
-                <input
-                  type="text"
-                  name="email-address"
-                  id="email-address"
-                  autoComplete="email"
-                  className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                />
+                <div className="relative mt-1 rounded-md shadow-sm">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="mt-1 block w-full rounded-md border border-cu-red py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="you@example.com"
+                    defaultValue="adamwathan"
+                    aria-invalid="true"
+                    aria-describedby="email-error"
+                  />
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                    <ExclamationCircleIcon
+                      className="h-5 w-5 text-red-500"
+                      aria-hidden="true"
+                    />
+                  </div>
+                </div>
+                <p className="mt-2 text-sm text-red-600" id="email-error">
+                  Please enter a valid e-mail address
+                </p>
               </div>
 
               <div className="col-span-6 sm:col-span-3">
