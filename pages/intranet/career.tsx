6d8345414ab8cf613227ab2955055edf0f63d46d
Niranjith Perumal <niranjithperumal@cunet.carleton.ca>
2022-09-14 16:31:49 -0400

    feature(career): added career layout

--- pages/intranet/career.tsx
+++ pages/intranet/career.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Featured from "../../components/Featured/Featured";
-import Footer from "../../components/Footer/Footer";
 import FooterCurrent from "../../components/Footer/FooterCurrent";
-import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
-import Spotlight from "../../components/Spotlight/Spotlight";
+import Profile from "../../components/Intranet/Profile";
+import Jobs from "../../components/Intranet/Jobs";
 
 const IntranetCareer: NextPage = () => {
   return (
@@ -15,9 +13,8 @@ const IntranetCareer: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <Header />
-      <Spotlight />
-      <Featured />
+      <Profile />
+      <Jobs/>
       <FooterCurrent />
     </>
   );
