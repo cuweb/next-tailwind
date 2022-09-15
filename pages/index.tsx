d1e295d3aa155b47822d28145eb04ecb676d3db0
Michael Corkum <mcorkum@gmail.com>
2022-09-15 14:43:25 -0400

    More changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
@@ -12,15 +12,15 @@ const Home: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{" "}
+          Welcome to{' '}
           <a className="text-red-carleton" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="rounded-md bg-gray-carleton p-3 font-mono text-lg text-white">
+          Get started by editing{' '}
+          <code className="bg-gray-carleton rounded-md p-3 font-mono text-lg text-white">
             pages/index.tsx
           </code>
         </p>
@@ -75,7 +75,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
