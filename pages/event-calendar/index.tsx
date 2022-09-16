0e232b98e1def11f14a75e60e67e1aff99098b53
Troy Chaplin <troy.chaplin@gmail.com>
2022-09-16 16:54:53 -0400

    Ran yarn build to test, fixed issue

--- pages/event-calendar/index.tsx
+++ pages/event-calendar/index.tsx
@@ -1,29 +1,29 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Featured from "../../components/Featured/Featured";
-import Footer from "../../components/Footer/Footer";
+import Footer from "../../components/Footer/FooterStandard";
 import FooterCurrent from "../../components/Footer/FooterCurrent";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
 import Spotlight from "../../components/Spotlight/Spotlight";
 
 const EventCalendar: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Event Calendar | Carleton University</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
-      <Header />
-      <Spotlight />
-      <Featured />
-      <Footer />
-      <br />
-      <br />
-      <FooterCurrent />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Event Calendar | Carleton University</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Navbar />
+			<Header />
+			<Spotlight />
+			<Featured />
+			<Footer />
+			<br />
+			<br />
+			<FooterCurrent />
+		</>
+	);
 };
 
 export default EventCalendar;
