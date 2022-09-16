9c066679f78908aab9e8d4316ff7bbd4ae95c5ab
Ish <ishdeep.711@gmail.com>
2022-09-15 23:56:07 -0400

    adding the hr page

--- pages/intranet/newsfeed.tsx
+++ pages/intranet/newsfeed.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from "next";
+import React from "react";
 import Navbar from "../../components/Intranet/Navbar";
+import Profile from "../../components/Intranet/Profile";
 
 const newsfeed: NextPage = () => {
   return (
     <>
       <Navbar />
+      <Profile />
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:py-10 lg:px-8">
         <h1 className="text-cu-black:900 text-3xl font-medium tracking-tight">
           Community & News
