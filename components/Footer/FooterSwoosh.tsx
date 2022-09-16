9ba5ecff7b253e65670e872364c2d0a02b1efc76
Troy Chaplin <troy.chaplin@gmail.com>
2022-09-16 16:53:07 -0400

    Updated footer, make swoosh component

---
+++ components/Footer/FooterSwoosh.tsx
@@ -0,0 +1,32 @@
+const FooterSwoosh = () => {
+	return (
+		// <div className="mx-auto max-w-7xl bg-[url('https://carleton.ca/assets/waves.svg')] bg-[length:100%] bg-bottom bg-no-repeat px-4 py-4">
+
+		<div className="grid justify-center bg-[url('https://carleton.ca/assets/waves.svg')] bg-[length:100%] bg-bottom bg-no-repeat pb-36 text-center text-base lg:pb-44">
+			<img
+				className="w-auto mx-auto mb-6 h-44"
+				src="/cu-logo-stacked-white-outline.svg"
+				alt="Carleton"
+			/>
+			<ul className="flex justify-center text-gray-400">
+				<li className="px-4" key="privacy">
+					<a href="#" className="hover:text-white">
+						Privacy Policy
+					</a>
+				</li>
+				<li className="px-4" key="accessibility">
+					<a href="#" className="hover:text-white">
+						Accessibility
+					</a>
+				</li>
+				<li className="px-4" key="copyright">
+					<a href="#" className="hover:text-white">
+						&copy; Copyright 2022
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default FooterSwoosh;
