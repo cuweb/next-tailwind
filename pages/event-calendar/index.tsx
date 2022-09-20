87e06decef609526a28bdc6b2127181a692d9eb1
Troy Chaplin <troy.chaplin@gmail.com>
2022-09-20 11:13:24 -0400

    Added basic card templates

--- pages/event-calendar/index.tsx
+++ pages/event-calendar/index.tsx
@@ -3,7 +3,10 @@ import Head from "next/head";
 import Featured from "../../components/Featured/Featured";
 import Footer from "../../components/Footer/FooterStandard";
 import Navbar from "../../components/EventCalendar/Navbar";
+import Container from "../layouts/components/_container";
+import Columns from "../layouts/components/_columns";
 import FeaturedEvents from "../../components/EventCalendar/FeaturedEvents";
+import EventCard from "../../components/Cards/EventCard";
 
 const EventCalendar: NextPage = () => {
 	return (
@@ -13,8 +16,32 @@ const EventCalendar: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Navbar />
-			<FeaturedEvents />
-			<Featured />
+
+			<header className="max-w-4xl px-8 py-12 mx-auto">
+				<h1 className="text-3xl font-medium tracking-tight text-cu-black-800 md:text-4xl">
+					Grids &amp; Layouts
+				</h1>
+			</header>
+
+			<main>
+				<Container bgColor="gray">
+					<Columns columns="4" maxWidth="7xl">
+						<EventCard title="" content="" />
+						<EventCard
+							title="Sed sollicitudin orci nisi, non tempus risus posuere vel"
+							content=""
+						/>
+						<EventCard
+							title=""
+							content="Suspendisse sed nibh non lorem ultrices auctor. Phasellus eget leo enim. Donec sit amet venenatis mi. Sed interdum nibh quis quam hendrerit tincidunt. Donec quis consectetur augue. Maecenas non felis malesuada, aliquet risus eget, consectetur leo. Sed fringilla at nisl nec rutrum."
+						/>
+						<EventCard title="" content="" />
+					</Columns>
+				</Container>
+				{/* <FeaturedEvents /> */}
+				{/* <Featured /> */}
+			</main>
+
 			<Footer />
 		</>
 	);
