75e1b437e4ae60d04480c6199935d16f24878b2b
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2022-09-22 13:36:51 -0400

    add events and date picker

--- pages/event-calendar/index.tsx
+++ pages/event-calendar/index.tsx
@@ -8,47 +8,46 @@ import Columns from "../../build-files/layouts/Columns/Columns";
 import FeaturedEvents from "../../components/EventCalendar/FeaturedEvents";
 import EventCard from "../../components/Cards/EventCard";
 import EventFilter from "../../components/EventCalendar/EventFilter";
+import EventWithDatePicker from "../../components/EventCalendar/EventWithDatePicker";
 const EventCalendar: NextPage = () => {
-	return (
-		<>
-			<Head>
-				<title>Event Calendar | Carleton University</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Navbar />
+  return (
+    <>
+      <Head>
+        <title>Event Calendar | Carleton University</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
 
-			<header className="max-w-4xl px-8 py-12 mx-auto">
-				<h1 className="text-3xl font-medium tracking-tight text-cu-black-800 md:text-4xl">
-					Grids &amp; Layouts
-				</h1>
-			</header>
+      <header className="mx-auto max-w-4xl px-8 py-12">
+        <h1 className="text-3xl font-medium tracking-tight text-cu-black-800 md:text-4xl">
+          Grids &amp; Layouts
+        </h1>
+      </header>
 
-			<main>
-				<Container bgColor="gray">
-					<Columns
-						columns="sm:grid-cols-2 lg:grid-cols-4"
-						maxWidth="7xl"
-					>
-						<EventCard title="" content="" />
-						<EventCard
-							title="Sed sollicitudin orci nisi, non tempus risus posuere vel"
-							content=""
-						/>
-						<EventCard
-							title=""
-							content="Suspendisse sed nibh non lorem ultrices auctor. Phasellus eget leo enim. Donec sit amet venenatis mi. Sed interdum nibh quis quam hendrerit tincidunt. Donec quis consectetur augue. Maecenas non felis malesuada, aliquet risus eget, consectetur leo. Sed fringilla at nisl nec rutrum."
-						/>
-						<EventCard title="" content="" />
-					</Columns>
-				</Container>
-				<EventFilter />
-				{/* <FeaturedEvents /> */}
-				{/* <Featured /> */}
-			</main>
+      <main>
+        <Container bgColor="gray">
+          <Columns columns="sm:grid-cols-2 lg:grid-cols-4" maxWidth="7xl">
+            <EventCard title="" content="" />
+            <EventCard
+              title="Sed sollicitudin orci nisi, non tempus risus posuere vel"
+              content=""
+            />
+            <EventCard
+              title=""
+              content="Suspendisse sed nibh non lorem ultrices auctor. Phasellus eget leo enim. Donec sit amet venenatis mi. Sed interdum nibh quis quam hendrerit tincidunt. Donec quis consectetur augue. Maecenas non felis malesuada, aliquet risus eget, consectetur leo. Sed fringilla at nisl nec rutrum."
+            />
+            <EventCard title="" content="" />
+          </Columns>
+        </Container>
+        <EventFilter />
+        <EventWithDatePicker />
+        {/* <FeaturedEvents /> */}
+        {/* <Featured /> */}
+      </main>
 
-			<Footer />
-		</>
-	);
+      <Footer />
+    </>
+  );
 };
 
 export default EventCalendar;
