d96435ae95c95a5bd2989f30982c4803fbc298a1
Troy Chaplin <troy.chaplin@gmail.com>
2022-09-23 14:57:50 -0400

    Few more updates

--- build-files/components/Banners/Page.tsx
+++ build-files/components/Banners/Page.tsx
@@ -2,7 +2,7 @@ export default function PageBanner({ children }: any) {
 	return (
 		<header className="bg-cu-black-50">
 			<div className="flex items-start px-6 pt-8 mx-auto max-w-7xl md:pt-12 lg:px-8">
-				<h1 className="text-3xl font-medium tracking-tight text-cu-black-800 md:text-4xl">
+				<h1 className="text-3xl font-medium text-cu-black-800 md:text-4xl">
 					{children}
 				</h1>
 			</div>
