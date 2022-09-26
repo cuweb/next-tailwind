a5cc409c6fc752c2b33f76868146889c75aa921a
Michael Corkum <mcorkum@gmail.com>
2022-09-26 14:20:34 -0400

    Adding images to events, adding category to event list, update event banner

--- components/Banner/FeaturedEventBanner.tsx
+++ components/Banner/FeaturedEventBanner.tsx
@@ -1,27 +1,27 @@
 export default function FeaturedEventBanner({ children }: any) {
-	return (
-		<header className="relative pb-32 bg-gray-800">
-			<div className="absolute inset-0">
-				<img
-					className="object-cover w-full h-full"
-					src="https://carleton.ca/about/wp-content/uploads/about-header-1600w-3.jpg"
-					alt=""
-				/>
-				<div
-					className="absolute inset-0 bg-cu-black-800 mix-blend-multiply"
-					aria-hidden="true"
-				/>
-			</div>
-			<div className="relative px-8 pt-12 pb-20 mx-auto max-w-7xl md:py-24">
-				<h1 className="text-4xl font-semibold text-white md:text-5xl">
-					Featured Events
-				</h1>
-				<p className="max-w-3xl mt-6 text-xl text-gray-300">
+  return (
+    <header className="bg-gray-800 pb-32">
+      {/* <div className="absolute inset-0">
+        <img
+          className="h-full w-full object-cover"
+          src="https://carleton.ca/about/wp-content/uploads/about-header-1600w-3.jpg"
+          alt=""
+        />
+        <div
+          className="absolute inset-0 bg-cu-black-800 mix-blend-multiply"
+          aria-hidden="true"
+        />
+      </div> */}
+      <div className="relative mx-auto max-w-7xl px-8 py-16">
+        <h1 className="text-4xl font-semibold text-white md:text-5xl">
+          Featured Events
+        </h1>
+        {/* <p className="max-w-3xl mt-6 text-xl text-gray-300">
 					Varius facilisi mauris sed sit. Non sed et duis dui leo,
 					vulputate id malesuada. Cras aliquet purus dui laoreet diam
 					sed lacus, fames.
-				</p>
-			</div>
-		</header>
-	);
+				</p> */}
+      </div>
+    </header>
+  );
 }
