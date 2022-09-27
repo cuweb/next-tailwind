7b9c78f2c7cbfe6ad665767a3a7ce7b3e41a21d3
Troy Chaplin <troy.chaplin@gmail.com>
2022-09-27 15:52:50 -0400

    Updated job list

--- _rds-final/components/ListingItem/FileItem/FileItem.tsx
+++ _rds-final/components/ListingItem/FileItem/FileItem.tsx
@@ -25,7 +25,7 @@ export default function FileItem(props: any) {
 											d="M4 23V1h10.84L20 6.473V23z"
 											fill="#fff"
 										></path>
-										<g fill="#bf112b">
+										<g fill="#E91C24">
 											<path d="M14.62 1.611l4.809 5.142v15.636H4.57V1.61zM14.858 1H4v22h16V6.5z"></path>
 											<path
 												d="M14 7V1h.195L20 6.805V7z"
@@ -36,7 +36,7 @@ export default function FileItem(props: any) {
 									</g>
 									<path
 										d="M16 15.34c0-1.98-8-.117-8 2.263 0 .192.135.397.353.397 1.675 0 4.779-11 3.26-11s.859 9.023 3.66 9.023c.408 0 .727-.167.727-.683z"
-										stroke="#bf112b"
+										stroke="#E91C24"
 										strokeWidth=".5"
 									></path>
 								</g>
