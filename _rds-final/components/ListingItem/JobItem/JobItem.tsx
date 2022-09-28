e03e4e16dbb59c745f7a5d422f28750626e07542
Troy Chaplin <troy.chaplin@gmail.com>
2022-09-28 06:30:17 -0400

    Updated job item listing

--- _rds-final/components/ListingItem/JobItem/JobItem.tsx
+++ _rds-final/components/ListingItem/JobItem/JobItem.tsx
@@ -18,25 +18,21 @@ export default function JobItem(props: any) {
 						{/* Extend touch target to entire panel */}
 						<span className="absolute inset-0" aria-hidden="true" />
 						{title}
+						<span className="italic font-light"> at {type}</span>
 					</a>
 				</h3>
-				<p className="flex items-center text-xs text-gray-500">
+				<p className="flex text-xs text-gray-500">
 					<CalendarIcon
-						className="mr-1.5 h-4 w-4 flex-shrink-0 text-cu-red"
+						className="mr-1.5 h-4 w-4 text-cu-red"
 						aria-hidden="true"
 					/>
-					Closes&nbsp;{""}
 					<time className="mr-4" dateTime={closeDate}>
-						{closeDateFull}
+						Closes {closeDateFull}
 					</time>
 				</p>
 			</div>
-
-			<div className="ml-auto">
-				<Badge.XSmall>{type}</Badge.XSmall>
-			</div>
 			<ChevronRightIcon
-				className="w-5 h-5 ml-4 text-cu-black-300"
+				className="w-5 h-5 ml-auto text-cu-black-300"
 				aria-hidden="true"
 			/>
 		</li>
