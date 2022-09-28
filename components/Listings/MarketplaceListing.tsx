9cc173181eef82c0bf94d32e013ce576fe41fce8
Troy Chaplin <troy.chaplin@gmail.com>
2022-09-27 20:30:42 -0400

    Updated marketplace

--- components/Listings/MarketplaceListing.tsx
+++ components/Listings/MarketplaceListing.tsx
@@ -5,80 +5,47 @@ const marketplace = [
 	{
 		name: "Nintendo 64",
 		link: "#",
-		handle: "10-05-2022",
-		location: "405 Robertson Hall",
-		dateFull: "January 7, 2020",
-		time: "$100.00",
-		imageUrl:
-			"https://images.unsplash.com/photo-1507878866276-a947ef722fee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80",
+		cost: "$100.00",
+		image: "https://source.unsplash.com/random/400x400",
 	},
 	{
 		name: "Plastic Boxes",
-		handle: "10-05-2022",
 		link: "#",
-		location: "405 Robertson Hall",
-		time: "$40.00",
-		dateFull: "January 7, 2020",
-		imageUrl:
-			"https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+		cost: "$40.00",
+		image: "https://source.unsplash.com/random/400x400",
 	},
 	{
 		name: "Cambridge Audio Stereo Amplifier",
-		handle: "10-05-2022",
-		dateFull: "January 7, 2020",
 		link: "#",
-		location: "405 Robertson Hall",
-		time: "$10000.00",
-		imageUrl:
-			"https://images.unsplash.com/photo-1550305080-4e029753abcf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80",
+		cost: "$10000.00",
+		image: "https://source.unsplash.com/random/400x400",
 	},
 	{
 		name: "IKEA Baskets",
-		handle: "10-05-2022",
-		dateFull: "January 7, 2020",
 		link: "#",
-		location: "405 Robertson Hall",
-		time: "$90.00",
-		imageUrl:
-			"https://images.unsplash.com/photo-1587825140708-dfaf72ae4b04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+		cost: "$90.00",
+		image: "https://source.unsplash.com/random/400x400",
 	},
 	{
 		name: "Hair Collection",
-		handle: "10-05-2022",
 		link: "#",
-		location: "405 Robertson Hall",
-		time: "$100.00",
-		dateFull: "January 7, 2020",
-		imageUrl:
-			"https://images.unsplash.com/photo-1507878866276-a947ef722fee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80",
+		cost: "$100.00",
+		image: "https://source.unsplash.com/random/400x400",
 	},
 ];
 
 export default function Marketplace() {
 	return (
 		<StackedList.Dividers>
-			{marketplace.map(
-				({
-					name,
-					handle,
-					link,
-					location,
-					time,
-					dateFull,
-					imageUrl,
-				}) => (
-					<MarketplaceItem
-						key={name}
-						name={name}
-						handle={handle}
-						link={link}
-						location={location}
-						time={time}
-						dateFull={dateFull}
-						imageUrl={imageUrl}
-					/>
-				)
-			)}
+			{marketplace.map(({ name, link, cost, image }) => (
+				<MarketplaceItem
+					key={name}
+					name={name}
+					link={link}
+					cost={cost}
+					image={image}
+				/>
+			))}
 		</StackedList.Dividers>
 	);
 }
--- components/Listings/NewsListing.tsx
+++ components/Listings/NewsListing.tsx
@@ -3,31 +3,28 @@ import NewsItem from "../../_rds-final/components/ListingItem/NewsItem/NewsItem"
 
 const posts = [
 	{
-		id: 1,
 		name: "Lived Experience Luncheon Series",
 		excerpt:
 			"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae. ",
 		date: "January 7, 2020",
 		link: "#",
-		image: "https://images.unsplash.com/photo-1507878866276-a947ef722fee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80",
+		image: "https://source.unsplash.com/random/400x300",
 	},
 	{
-		id: 2,
 		name: "Science Student Alumni Mixer 2022",
 		excerpt:
 			"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae. ",
 		date: "January 7, 2020",
 		link: "#",
-		image: "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+		image: "https://source.unsplash.com/random/400x300",
 	},
 	{
-		id: 3,
 		name: "Milestone Anniversary Celebration 2022",
 		excerpt:
 			"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae. ",
 		date: "January 7, 2020",
 		link: "#",
-		image: "https://images.unsplash.com/photo-1550305080-4e029753abcf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80",
+		image: "https://source.unsplash.com/random/400x300",
 	},
 	{
 		name: "Virtual Career Fairs",
@@ -35,7 +32,7 @@ const posts = [
 			"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae. ",
 		date: "January 7, 2020",
 		link: "#",
-		image: "https://images.unsplash.com/photo-1587825140708-dfaf72ae4b04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+		image: "https://source.unsplash.com/random/400x300",
 	},
 	{
 		name: "80 Years of Carleton University",
@@ -43,7 +40,7 @@ const posts = [
 			"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae. ",
 		date: "January 7, 2020",
 		link: "#",
-		image: "https://images.unsplash.com/photo-1507878866276-a947ef722fee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80",
+		image: "https://source.unsplash.com/random/400x300",
 	},
 ];
 
