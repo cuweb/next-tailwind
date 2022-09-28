a6f27e5479b48ae50566abbcd066b45604cfee50
Troy Chaplin <troy.chaplin@gmail.com>
2022-09-28 10:31:40 -0400

    Adding list item template to use to make others consistent

--- _rds-final/components/ListingItem/FileItem/FileItem.tsx
+++ _rds-final/components/ListingItem/FileItem/FileItem.tsx
@@ -5,69 +5,67 @@ export default function FileItem(props: any) {
 		props;
 
 	return (
-		<>
-			<li key={name}>
-				<div className="relative flex items-center p-4 hover:bg-gray-50 sm:p-6">
-					<div className="flex items-start lg:items-center">
-						<svg
-							className="flex items-start flex-none w-10 h-10"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-							aria-hidden="true"
-						>
+		<li key={name}>
+			<div className="relative flex items-center p-4 hover:bg-gray-50 sm:p-6">
+				<div className="flex items-start lg:items-center">
+					<svg
+						className="flex items-start flex-none w-10 h-10"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+						aria-hidden="true"
+					>
+						<g>
 							<g>
-								<g>
-									<g fill="none" fillRule="evenodd">
-										<g fillRule="nonzero">
-											<path
-												d="M4 23V1h10.84L20 6.473V23z"
-												fill="#fff"
-											></path>
-											<g fill="#E91C24">
-												<path d="M14.62 1.611l4.809 5.142v15.636H4.57V1.61zM14.858 1H4v22h16V6.5z"></path>
-												<path
-													d="M14 7V1h.195L20 6.805V7z"
-													fillOpacity=".215"
-												></path>
-												<path d="M14.6 1.848L19.152 6.4H14.6zm0-.848H14v6h6v-.6z"></path>
-											</g>
-										</g>
+								<g fill="none" fillRule="evenodd">
+									<g fillRule="nonzero">
 										<path
-											d="M16 15.34c0-1.98-8-.117-8 2.263 0 .192.135.397.353.397 1.675 0 4.779-11 3.26-11s.859 9.023 3.66 9.023c.408 0 .727-.167.727-.683z"
-											stroke="#E91C24"
-											strokeWidth=".5"
+											d="M4 23V1h10.84L20 6.473V23z"
+											fill="#fff"
 										></path>
+										<g fill="#E91C24">
+											<path d="M14.62 1.611l4.809 5.142v15.636H4.57V1.61zM14.858 1H4v22h16V6.5z"></path>
+											<path
+												d="M14 7V1h.195L20 6.805V7z"
+												fillOpacity=".215"
+											></path>
+											<path d="M14.6 1.848L19.152 6.4H14.6zm0-.848H14v6h6v-.6z"></path>
+										</g>
 									</g>
+									<path
+										d="M16 15.34c0-1.98-8-.117-8 2.263 0 .192.135.397.353.397 1.675 0 4.779-11 3.26-11s.859 9.023 3.66 9.023c.408 0 .727-.167.727-.683z"
+										stroke="#E91C24"
+										strokeWidth=".5"
+									></path>
 								</g>
 							</g>
-						</svg>
-						<div className="px-4">
-							<h3 className="text-sm font-semibold text-gray-800">
-								<a
-									href={link}
-									className="hover:text-cu-red focus:outline-none"
-								>
-									{/* Extend touch target to entire panel */}
-									<span
-										className="absolute inset-0"
-										aria-hidden="true"
-									/>
-									{name}
-								</a>
-							</h3>
-							<div className="flex flex-wrap items-center">
-								<p className="mt-1 mb-1 mr-4 text-sm italic text-cu-black-400">
-									{preview} - {dateFull}
-								</p>
-							</div>
+						</g>
+					</svg>
+					<div className="px-4">
+						<h3 className="text-sm font-semibold text-gray-800">
+							<a
+								href={link}
+								className="hover:text-cu-red focus:outline-none"
+							>
+								{/* Extend touch target to entire panel */}
+								<span
+									className="absolute inset-0"
+									aria-hidden="true"
+								/>
+								{name}
+							</a>
+						</h3>
+						<div className="flex flex-wrap items-center">
+							<p className="mt-1 mb-1 mr-4 text-sm italic text-cu-black-400">
+								{preview} - {dateFull}
+							</p>
 						</div>
 					</div>
-					<ChevronRightIcon
-						className="flex-none w-5 h-5 ml-auto text-cu-black-300"
-						aria-hidden="true"
-					/>
 				</div>
-			</li>
-		</>
+				<ChevronRightIcon
+					className="flex-none w-5 h-5 ml-auto text-cu-black-300"
+					aria-hidden="true"
+				/>
+			</div>
+		</li>
 	);
 }
---
+++ _rds-final/components/ListingItem/TemplateItem/TemplateItem.tsx
@@ -0,0 +1,27 @@
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+
+export default function TemplateItem(props: any) {
+	const { name, date, excerpt, link, image } = props;
+	return (
+		<li key={name}>
+			<div className="relative flex items-center gap-2 p-6 hover:bg-gray-50">
+				<div className="flex items-start gap-2">
+					<div className="flex-none w-16 bg-blue-200 md:w-20">
+						Img/Icn
+					</div>
+
+					<div className="flex-auto bg-blue-500">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Duis vitae enim vitae ligula elementum pellentesque
+						vitae eget orci. Vestibulum ultricies turpis metus
+					</div>
+				</div>
+
+				<ChevronRightIcon
+					className="flex-none w-5 h-5 ml-auto bg-blue-200 text-cu-black-300"
+					aria-hidden="true"
+				/>
+			</div>
+		</li>
+	);
+}
