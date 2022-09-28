ed84b555b28e5c861a2d408ec30e0302d39d027d
Troy Chaplin <troy.chaplin@gmail.com>
2022-09-28 12:05:48 -0400

    Updated event item nav to use template

--- _rds-final/components/ListingItem/EventItem/EventItem.tsx
+++ _rds-final/components/ListingItem/EventItem/EventItem.tsx
@@ -6,66 +6,58 @@ import {
 import Badge from "../../Badge/Badge";
 
 export default function EventItem(props: any) {
-	const { name, link, location, time, month, day, dateFull } = props;
+	const { title, link, location, time, month, day, date } = props;
 
 	return (
-		<li
-			key={name}
-			className="relative flex items-center p-6 hover:bg-gray-50"
-		>
-			<div className="flex">
-				<div className="flex flex-col justify-center flex-none w-16 h-16 text-center shadow rounded-2xl bg-gray-50 lg:h-20 lg:w-20">
-					<p className="text-xs font-bold uppercase text-cu-red">
-						{month}
-					</p>
-					<p className="text-2xl font-bold uppercase text-cu-black-900">
-						{day}
-					</p>
-				</div>
-				<div className="px-5">
-					<h3 className="mb-2 text-base font-semibold text-gray-800">
-						<a
-							href={link}
-							className="hover:text-cu-red focus:outline-none"
-						>
-							{/* Extend touch target to entire panel */}
-							<span
-								className="absolute inset-0"
-								aria-hidden="true"
-							/>
-							{name}
-						</a>
-					</h3>
+		<li key={title}>
+			<div className="relative flex items-center gap-2 p-6 hover:bg-gray-50">
+				<div className="flex flex-col items-start gap-3 sm:items-center md:flex-row">
+					<div className="flex-none w-16 md:w-20">Img/Icn</div>
 
-					<ul className="flex flex-wrap sm:gap-2">
-						<li className="flex items-center mr-2 text-sm text-gray-500">
-							<ClockIcon
-								className="mr-1.5 h-5 w-5 flex-shrink-0 text-cu-red-300"
-								aria-hidden="true"
-							/>
+					<div className="flex-auto">
+						<h3 className="mb-2 text-base font-semibold text-gray-800">
+							<a
+								href={link}
+								className="hover:text-cu-red focus:outline-none"
+							>
+								<span
+									className="absolute inset-0"
+									aria-hidden="true"
+								/>
+								{title}
+							</a>
+						</h3>
+						<ul className="flex flex-wrap sm:gap-2">
+							<li className="flex items-center mr-2 text-sm text-gray-500">
+								<ClockIcon
+									className="mr-1.5 h-5 w-5 flex-shrink-0 text-cu-red-300"
+									aria-hidden="true"
+								/>
 
-							<time dateTime={dateFull}>{time}</time>
-						</li>
-						<li className="flex items-center mt-2 text-sm text-gray-500 sm:mt-0">
-							<MapPinIcon
-								className="mr-1.5 h-5 w-5 flex-shrink-0 text-cu-red-300"
-								aria-hidden="true"
-							/>
-							{location}
-						</li>
-					</ul>
+								<time dateTime={date}>{time}</time>
+							</li>
+							<li className="flex items-center mt-2 text-sm text-gray-500 sm:mt-0">
+								<MapPinIcon
+									className="mr-1.5 h-5 w-5 flex-shrink-0 text-cu-red-300"
+									aria-hidden="true"
+								/>
+								{location}
+							</li>
+						</ul>
 
-					<div className="mt-2">
-						<Badge.XSmall>Multi-Day</Badge.XSmall>
-						<Badge.XSmall>Student Event</Badge.XSmall>
-						<Badge.XSmall>Faculty Luncheon</Badge.XSmall>
+						<div className="mt-2">
+							<Badge.XSmall>Multi-Day</Badge.XSmall>
+							<Badge.XSmall>Student Event</Badge.XSmall>
+							<Badge.XSmall>Faculty Luncheon</Badge.XSmall>
+						</div>
 					</div>
 				</div>
+
+				<ChevronRightIcon
+					className="flex-none w-5 h-5 ml-auto text-cu-black-300"
+					aria-hidden="true"
+				/>
 			</div>
-			<ChevronRightIcon
-				className="flex-none w-5 h-5 ml-auto text-cu-black-300"
-				aria-hidden="true"
-			/>
 		</li>
 	);
 }
