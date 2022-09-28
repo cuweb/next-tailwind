56c3e04be2ce6a92e63d430dd5c33fa87fda77b9
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2022-09-28 09:19:13 -0400

    add submit link

--- _rds-final/components/Navbar/EventNavbar.tsx
+++ _rds-final/components/Navbar/EventNavbar.tsx
@@ -69,6 +69,12 @@ export default function Example() {
                   >
                     Single 3
                   </a>
+				  <a
+                    href="/events/submit"
+                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-cu-black-100 hover:text-cu-red"
+                  >
+                    Submit
+                  </a>
                 </div>
               </div>
               <div className="flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-end">
