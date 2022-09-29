089b2f87db38ff79450f0ae955848c50ce3686f0
Michael Corkum <mcorkum@gmail.com>
2022-09-29 13:50:44 -0400

    Spacing matching the homepage after looking on a PC

--- components/Banner/EventBanner.tsx
+++ components/Banner/EventBanner.tsx
@@ -1,14 +1,14 @@
-import GradientDiv from "../../_rds-final/_misc/Gradient/GradientDiv";
+import GradientDiv from '../../_rds-final/_misc/Gradient/GradientDiv';
 
 export default function EventBanner({ children }: any) {
-	return (
-		<header className="relative bg-cu-black-50">
-			<GradientDiv />
-			<div className="relative items-start px-8 py-8 mx-auto max-w-7xl lg:py-16 lg:px-8">
-				<h1 className="text-3xl font-medium text-cu-black-800 lg:text-4xl">
-					{children}
-				</h1>
-			</div>
-		</header>
-	);
+  return (
+    <header className="relative bg-cu-black-50">
+      <GradientDiv />
+      <div className="relative mx-auto max-w-7xl items-start px-8 py-8 lg:py-16 lg:px-8">
+        <h1 className="text-3xl font-medium text-cu-black-800 lg:text-4xl">
+          {children}
+        </h1>
+      </div>
+    </header>
+  );
 }
--- components/Banner/FeaturedEventBanner.tsx
+++ components/Banner/FeaturedEventBanner.tsx
@@ -1,11 +1,11 @@
-import GradientDiv from "../../_rds-final/_misc/Gradient/GradientDiv";
+import GradientDiv from '../../_rds-final/_misc/Gradient/GradientDiv';
 
 export default function FeaturedEventBanner({ children }: any) {
-	return (
-		<header className="relative pb-32">
-			{/* <header className="relative pb-32 bg-gray-800"> */}
-			<GradientDiv />
-			{/* <div className="absolute inset-0">
+  return (
+    <header className="relative pb-32">
+      {/* <header className="relative pb-32 bg-gray-800"> */}
+      <GradientDiv />
+      {/* <div className="absolute inset-0">
         <img
           className="object-cover w-full h-full"
           src="https://carleton.ca/about/wp-content/uploads/about-header-1600w-3.jpg"
@@ -16,16 +16,16 @@ export default function FeaturedEventBanner({ children }: any) {
           aria-hidden="true"
         />
       </div> */}
-			<div className="relative px-8 py-12 mx-auto max-w-7xl lg:py-20">
-				<h1 className="text-3xl font-semibold text-cu-black-800 md:text-5xl">
-					Featured Events
-				</h1>
-				{/* <p className="max-w-3xl mt-6 text-xl text-gray-300">
+      <div className="relative mx-auto max-w-7xl px-8 py-8 lg:py-16 lg:px-8">
+        <h1 className="text-3xl font-medium text-cu-black-800 lg:text-4xl">
+          Featured Events
+        </h1>
+        {/* <p className="max-w-3xl mt-6 text-xl text-gray-300">
 					Varius facilisi mauris sed sit. Non sed et duis dui leo,
 					vulputate id malesuada. Cras aliquet purus dui laoreet diam
 					sed lacus, fames.
 				</p> */}
-			</div>
-		</header>
-	);
+      </div>
+    </header>
+  );
 }
