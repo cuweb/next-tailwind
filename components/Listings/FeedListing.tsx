bc770228741342bd64b19b25205f42c951ee41f6
Troy Chaplin <troy.chaplin@gmail.com>
2022-10-04 10:02:31 -0400

    Updated content being fed into feed item listing

--- components/Listings/FeedListing.tsx
+++ components/Listings/FeedListing.tsx
@@ -11,33 +11,33 @@ const posts = [
 		excerpt:
 			"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae.",
 	},
-	// {
-	// 	id: 2,
-	// 	title: "New password policy",
-	// 	link: "https://carleton.ca",
-	// 	date: "September 9th, 2023",
-	// 	category: "Healthy Workplace",
-	// 	excerpt:
-	// 		"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae.",
-	// },
-	// {
-	// 	id: 3,
-	// 	title: "Office closed on July 2nd",
-	// 	link: "https://carleton.ca",
-	// 	date: "September 15th, 2023",
-	// 	category: "Potato",
-	// 	excerpt:
-	// 		"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae.",
-	// },
-	// {
-	// 	id: 2,
-	// 	title: "New password policy",
-	// 	link: "https://carleton.ca",
-	// 	date: "September 9th, 2023",
-	// 	category: "Healthy Workplace",
-	// 	excerpt:
-	// 		"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae.",
-	// },
+	{
+		id: 2,
+		title: "New password policy",
+		link: "https://carleton.ca",
+		date: "September 9th, 2023",
+		category: "Healthy Workplace",
+		excerpt:
+			"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae.",
+	},
+	{
+		id: 3,
+		title: "Office closed on July 2nd",
+		link: "https://carleton.ca",
+		date: "September 15th, 2023",
+		category: "Potato",
+		excerpt:
+			"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae.",
+	},
+	{
+		id: 2,
+		title: "New password policy",
+		link: "https://carleton.ca",
+		date: "September 9th, 2023",
+		category: "Healthy Workplace",
+		excerpt:
+			"Tenetur libero voluptatem rerum occaecati qui est molestiae exercitationem. Voluptate quisquam iure assumenda consequatur ex et recusandae.",
+	},
 ];
 
 export default function FeedListing() {
