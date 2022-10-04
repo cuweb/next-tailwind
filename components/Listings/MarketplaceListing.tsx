7977cfcfaf1c72fd38839874f663d35c84d1507c
Troy Chaplin <troy.chaplin@gmail.com>
2022-10-04 14:48:21 -0400

    Updated marketplace item component

--- components/Listings/MarketplaceListing.tsx
+++ components/Listings/MarketplaceListing.tsx
@@ -8,43 +8,46 @@ const marketplace = [
 		cost: "$100.00",
 		image: "https://source.unsplash.com/random/400x300",
 	},
-	{
-		title: "Plastic Boxes",
-		link: "#",
-		cost: "$40.00",
-		image: "https://source.unsplash.com/random/400x300",
-	},
-	{
-		title: "Cambridge Audio Stereo Amplifier",
-		link: "#",
-		cost: "$10000.00",
-		image: "https://source.unsplash.com/random/400x300",
-	},
-	{
-		title: "IKEA Baskets",
-		link: "#",
-		cost: "$90.00",
-		image: "https://source.unsplash.com/random/400x300",
-	},
-	{
-		title: "Hair Collection",
-		link: "#",
-		cost: "$100.00",
-		image: "https://source.unsplash.com/random/400x300",
-	},
+	// {
+	// 	title: "Plastic Boxes",
+	// 	link: "#",
+	// 	cost: "$40.00",
+	// 	image: "https://source.unsplash.com/random/400x300",
+	// },
+	// {
+	// 	title: "Cambridge Audio Stereo Amplifier",
+	// 	link: "#",
+	// 	cost: "$10000.00",
+	// 	image: "https://source.unsplash.com/random/400x300",
+	// },
+	// {
+	// 	title: "IKEA Baskets",
+	// 	link: "#",
+	// 	cost: "$90.00",
+	// 	image: "https://source.unsplash.com/random/400x300",
+	// },
+	// {
+	// 	title: "Hair Collection",
+	// 	link: "#",
+	// 	cost: "$100.00",
+	// 	image: "https://source.unsplash.com/random/400x300",
+	// },
 ];
 
 export default function MarketplaceListing() {
 	return (
 		<StackedList.Dividers>
 			{marketplace.map(({ title, link, cost, image }) => (
-				<MarketplaceItem
-					key={title}
-					title={title}
-					link={link}
-					cost={cost}
-					image={image}
-				/>
+				<MarketplaceItem key={title}>
+					<MarketplaceItem.Image image={image} />
+					<MarketplaceItem.Title
+						title={title}
+						link={link}
+						cost={cost}
+					>
+						<MarketplaceItem.Category />
+					</MarketplaceItem.Title>
+				</MarketplaceItem>
 			))}
 		</StackedList.Dividers>
 	);
