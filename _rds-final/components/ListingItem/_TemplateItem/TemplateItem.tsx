b3104a17ab62669a2fcd8e8cc1e5e5e31fe18f69
Troy Chaplin <troy.chaplin@gmail.com>
2022-10-10 11:48:57 -0400

    Updated listing template

--- _rds-final/components/ListingItem/_TemplateItem/TemplateItem.tsx
+++ _rds-final/components/ListingItem/_TemplateItem/TemplateItem.tsx
@@ -4,22 +4,20 @@ export default function TemplateItem(props: any) {
 	const { name, link } = props;
 	return (
 		<li key={name}>
-			<div className="relative flex items-center gap-2 p-6 hover:bg-gray-50">
+			<a
+				href={link}
+				className="relative flex items-center gap-2 p-6 cursor-pointer group hover:bg-gray-50 focus:outline-none"
+			>
 				<div className="flex items-start gap-3">
 					<div className="flex-none w-16 md:w-20">Img/Icn</div>
 
 					<div className="flex-auto">
-						<h3 className="text-sm font-semibold text-cu-black-800">
-							<a
-								href={link}
-								className="hover:text-cu-red focus:outline-none"
-							>
-								<span
-									className="absolute inset-0"
-									aria-hidden="true"
-								/>
-								{name}
-							</a>
+						<h3 className="text-sm font-semibold text-cu-black-800 group-hover:text-cu-red">
+							<span
+								className="absolute inset-0"
+								aria-hidden="true"
+							/>
+							{name}
 						</h3>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 						Duis vitae enim vitae ligula elementum pellentesque
@@ -31,7 +29,7 @@ export default function TemplateItem(props: any) {
 					className="flex-none w-5 h-5 ml-auto text-cu-black-300"
 					aria-hidden="true"
 				/>
-			</div>
+			</a>
 		</li>
 	);
 }
