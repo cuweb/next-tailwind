2d47c3860d3866c97abff679eb35b5d214c2c579
Troy Chaplin <troy.chaplin@gmail.com>
2022-10-24 23:43:36 -0400

    Added banner test page

---
+++ pages/misc/banners/index.tsx
@@ -0,0 +1,80 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+import NavBar from "../../../_rds-final/components/Navbar/Carleton";
+import FooterBasic from "../../../_rds-final/components/Footer/FooterBasic/FooterBasic";
+import FooterStandard from "../../../_rds-final/components/Footer/FooterStandard/FooterStandard";
+import FooterCondensed from "../../../_rds-final/components/Footer/FooterCondensed/FooterCondensed";
+import FooterSwoosh from "../../../_rds-final/components/Footer/FooterSwoosh/FooterSwoosh";
+import Container from "../../../_rds-final/layouts/Container/Container";
+import Banner from "../../../_rds-final/components/Banner/Banner/Banner";
+
+import { ArrowDownIcon } from "@heroicons/react/24/outline";
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>Carleton University | Home</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<NavBar />
+
+			<header className="bg-cu-black-50">
+				<div className="flex items-center justify-between px-8 py-8 mx-auto max-w-7xl lg:py-16 lg:px-8">
+					<h1 className="text-3xl font-medium text-cu-black-800 lg:text-4xl">
+						Default Banner
+					</h1>
+					<div className="flex gap-6">
+						<button
+							type="button"
+							aria-label="Add to My Events"
+							className="inline-flex items-center justify-center px-4 py-3 font-medium text-white rounded-md bg-cu-red hover:bg-cu-black-600 focus:outline-none"
+						>
+							<span className="mr-2"></span>
+							Primary Button
+						</button>
+						<button
+							type="button"
+							aria-label="Register Now"
+							className="inline-flex items-center justify-center px-4 py-3 font-medium bg-white border rounded-md false border-1 border-cu-red text-cu-red hover:bg-cu-red hover:text-white focus:outline-none"
+						>
+							<span className="mr-2"></span>
+							Secondary Button
+						</button>
+					</div>
+				</div>
+			</header>
+
+			<br />
+
+			<header className="bg-cu-black-50">
+				<div className="flex flex-col items-center justify-between gap-8 px-8 py-8 mx-auto max-w-7xl lg:py-16 lg:px-8">
+					<h1 className="text-3xl font-medium text-cu-black-800 lg:text-4xl">
+						Default Banner
+					</h1>
+					<div className="flex gap-6">
+						<button
+							type="button"
+							aria-label="Add to My Events"
+							className="inline-flex items-center justify-center px-4 py-3 font-medium text-white rounded-md bg-cu-red hover:bg-cu-black-600 focus:outline-none"
+						>
+							<span className="mr-2"></span>
+							Add to My Events
+						</button>
+						<button
+							type="button"
+							aria-label="Register Now"
+							className="inline-flex items-center justify-center px-4 py-3 font-medium bg-white border rounded-md false border-1 border-cu-red text-cu-red hover:bg-cu-red hover:text-white focus:outline-none"
+						>
+							<span className="mr-2"></span>
+							Register Now
+						</button>
+					</div>
+				</div>
+			</header>
+		</>
+	);
+};
+
+export default Home;
--- pages/misc/index.tsx
+++ pages/misc/index.tsx
@@ -7,9 +7,13 @@ import Container from "../../_rds-final/layouts/Container/Container";
 import Column from "../../_rds-final/layouts/Columns/Columns";
 import Banner from "../../_rds-final/components/Banner/Banner/Banner";
 
-import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
-
 const projects = [
+	{
+		name: "Banner Tests",
+		initials: "BN",
+		href: "/misc/banners",
+		bgColor: "bg-slate-600",
+	},
 	{
 		name: "Card Tests",
 		initials: "CT",
@@ -50,7 +54,7 @@ const Home: NextPage = () => {
 			<NavBar />
 
 			<Banner>
-				<Banner.Title title="Odds and Ends" />
+				<Banner.Title title="Misc Tests" />
 			</Banner>
 
 			<main>
