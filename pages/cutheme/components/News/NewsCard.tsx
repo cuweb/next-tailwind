17e6d9cdcce6fa826b692b38ad347373aec89328
Troy Chaplin <troy.chaplin@gmail.com>
2023-01-13 15:21:27 -0500

    Added not-prose to card

--- pages/cutheme/components/News/NewsCard.tsx
+++ pages/cutheme/components/News/NewsCard.tsx
@@ -6,7 +6,7 @@ const styles = {
 
 export default function NewsCard() {
 	return (
-		<div className="relative flex flex-col overflow-hidden duration-300 ease-in bg-white rounded-lg shadow-lg group hover:scale-105">
+		<div className="relative flex flex-col overflow-hidden duration-300 ease-in bg-white rounded-lg shadow-lg not-prose group hover:scale-105">
 			<a href="https://carleton.ca" className="cursor-pointer">
 				<img
 					className="object-cover w-full"
