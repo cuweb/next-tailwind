8b0dd753b5f30d230f139a7ed6dd2d2035043a26
Troy Chaplin <troy.chaplin@gmail.com>
2023-01-24 21:35:09 -0500

    Updated all block example pages

--- pages/cutheme/_components/Content/Content.Numbers.tsx
+++ pages/cutheme/_components/Content/Content.Numbers.tsx
@@ -1,7 +1,8 @@
+import { Column } from "rds-tailwind";
 import NumberCardCenter from "../Numbers/NumberCardCenter";
 import NumberCard from "../Numbers/NumbersCard";
 
-export default function ContentEvents() {
+export default function ContentNumbers() {
 	return (
 		<>
 			<h2>H2 Heading</h2>
@@ -21,41 +22,7 @@ export default function ContentEvents() {
 				varius sit neque erat velit.
 			</p>
 
-			<ul>
-				<li>Quis elit egestas venenatis mattis dignissim.</li>
-				<li>
-					Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-				</li>
-				<li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-			</ul>
-
-			<h3>H3 Heading</h3>
-
-			<p>
-				Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam
-				consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod
-				vitae interdum mauris enim, consequat vulputate nibh. Maecenas
-				pellentesque id sed tellus mauris, ultrices mauris. Tincidunt
-				enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
-				turpis ipsum eu a sed convallis diam.
-			</p>
-
-			<blockquote>
-				<p>
-					Sagittis scelerisque nulla cursus in enim consectetur quam.
-					Dictum urna sed consectetur neque tristique pellentesque.
-					Blandit amet, sed aenean erat arcu morbi.
-				</p>
-			</blockquote>
-
-			<p>
-				Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-				risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-				Id dolor praesent donec est. Odio penatibus risus viverra tellus
-				varius sit neque erat velit.
-			</p>
-
-			<h4>H4 Heading</h4>
+			<h3>2 col left aligned</h3>
 
 			<p>
 				Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>
@@ -66,7 +33,9 @@ export default function ContentEvents() {
 				congue erat ac. Cras fermentum convallis quam.
 			</p>
 
-			<div className="mx-auto grid grid-cols-1 gap-8 md:grid-cols-2 [&:last-child]:mb-0 [&+.cu-column]:mt-8 ">
+			<div className="my-10 mx-auto grid grid-cols-1 gap-8 md:grid-cols-2 [&:last-child]:mb-0 [&+.cu-column]:mt-8 ">
+				<NumberCard />
+				<NumberCard />
 				<NumberCard />
 				<NumberCard />
 			</div>
@@ -84,7 +53,20 @@ export default function ContentEvents() {
 				erat velit.
 			</p>
 
-			<div className="mx-auto grid grid-cols-1 gap-8 md:grid-cols-2 [&:last-child]:mb-0 [&+.cu-column]:mt-8 ">
+			<h3>2 col center aligned</h3>
+
+			<p>
+				Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>
+				. Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+				ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+				viverra ridiculus non molestie. Gravida quis fringilla amet eget
+				dui tempor dignissim. Facilisis auctor venenatis varius nunc,
+				congue erat ac. Cras fermentum convallis quam.
+			</p>
+
+			<div className="my-10 mx-auto grid grid-cols-1 gap-8 md:grid-cols-2 [&:last-child]:mb-0 [&+.cu-column]:mt-8 ">
+				<NumberCardCenter />
+				<NumberCardCenter />
 				<NumberCardCenter />
 				<NumberCardCenter />
 			</div>
@@ -110,7 +92,20 @@ export default function ContentEvents() {
 				pulvinar ipsum.
 			</p>
 
-			<div className="mx-auto grid grid-cols-1 gap-8 md:grid-cols-3 [&:last-child]:mb-0 [&+.cu-column]:mt-8 ">
+			<h3>3 col left aligned</h3>
+
+			<p>
+				Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>
+				. Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+				ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+				viverra ridiculus non molestie. Gravida quis fringilla amet eget
+				dui tempor dignissim. Facilisis auctor venenatis varius nunc,
+				congue erat ac. Cras fermentum convallis quam.
+			</p>
+
+			<div className="my-10 mx-auto grid grid-cols-1 gap-8 md:grid-cols-3 [&:last-child]:mb-0 [&+.cu-column]:mt-8 ">
+				<NumberCard />
+				<NumberCard />
 				<NumberCard />
 				<NumberCard />
 				<NumberCard />
@@ -125,15 +120,90 @@ export default function ContentEvents() {
 				volutpat mollis. Aenean mauris leo, congue id laoreet vitae,
 				bibendum at dui. Proin ac aliquet enim, a ullamcorper tortor. Ut
 				mattis, ante id porttitor elementum, lorem mauris rutrum enim,
+				ut tristique est est at turpis.
+			</p>
+
+			<h3>3 col left breaking outside a max width below 1024</h3>
+
+			<p>
+				Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>
+				. Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+				ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+				viverra ridiculus non molestie. Gravida quis fringilla amet eget
+				dui tempor dignissim. Facilisis auctor venenatis varius nunc,
+				congue erat ac. Cras fermentum convallis quam.
+			</p>
+
+			<Column cols="3" maxWidth="5xl">
+				<NumberCard />
+				<NumberCard />
+				<NumberCard />
+				<NumberCard />
+				<NumberCard />
+			</Column>
+
+			<p>
+				Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
+				risus enim. Mattis mauris semper sed amet vitae sed turpis id.
+				Id dolor praesent donec est. Odio penatibus risus viverra tellus
+				varius sit neque erat velit. Phasellus et lectus elementum,
+				dictum odio quis, euismod augue. Nulla ultricies orci eu
+				volutpat mollis. Aenean mauris leo, congue id laoreet vitae,
+				bibendum at dui. Proin ac aliquet enim, a ullamcorper tortor. Ut
+				mattis, ante id porttitor elementum, lorem mauris rutrum enim,
 				ut tristique est est at turpis. Nam hendrerit luctus ex, ac
 				rhoncus mauris pretium varius.
 			</p>
 
-			<div className="mx-auto grid grid-cols-1 gap-8 md:grid-cols-3 [&:last-child]:mb-0 [&+.cu-column]:mt-8 ">
+			<h3>3 col center aligned</h3>
+
+			<p>
+				Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>
+				. Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+				ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+				viverra ridiculus non molestie. Gravida quis fringilla amet eget
+				dui tempor dignissim. Facilisis auctor venenatis varius nunc,
+				congue erat ac. Cras fermentum convallis quam.
+			</p>
+
+			<div className="my-10 mx-auto grid grid-cols-1 gap-8 md:grid-cols-3 [&:last-child]:mb-0 [&+.cu-column]:mt-8 ">
+				<NumberCardCenter />
+				<NumberCardCenter />
 				<NumberCardCenter />
 				<NumberCardCenter />
 				<NumberCardCenter />
 			</div>
+
+			<p>
+				Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
+				risus enim. Mattis mauris semper sed amet vitae sed turpis id.
+				Id dolor praesent donec est. Odio penatibus risus viverra tellus
+				varius sit neque erat velit. Phasellus et lectus elementum,
+				dictum odio quis, euismod augue. Nulla ultricies orci eu
+				volutpat mollis. Aenean mauris leo, congue id laoreet vitae,
+				bibendum at dui. Proin ac aliquet enim, a ullamcorper tortor. Ut
+				mattis, ante id porttitor elementum, lorem mauris rutrum enim,
+				ut tristique est est at turpis.
+			</p>
+
+			<h3>3 col center breaking outside a max width below 1024</h3>
+
+			<p>
+				Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>
+				. Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+				ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+				viverra ridiculus non molestie. Gravida quis fringilla amet eget
+				dui tempor dignissim. Facilisis auctor venenatis varius nunc,
+				congue erat ac. Cras fermentum convallis quam.
+			</p>
+
+			<Column cols="3" maxWidth="5xl">
+				<NumberCardCenter />
+				<NumberCardCenter />
+				<NumberCardCenter />
+				<NumberCardCenter />
+				<NumberCardCenter />
+			</Column>
 
 			<p>
 				Phasellus et lectus elementum, dictum odio quis, euismod augue.
@@ -155,7 +225,22 @@ export default function ContentEvents() {
 				risus viverra tellus varius sit neque erat velit.
 			</p>
 
+			<h3>4 col left aligned</h3>
+
+			<p>
+				Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>
+				. Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+				ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+				viverra ridiculus non molestie. Gravida quis fringilla amet eget
+				dui tempor dignissim. Facilisis auctor venenatis varius nunc,
+				congue erat ac. Cras fermentum convallis quam.
+			</p>
+
 			<div className="cu-column mx-auto grid max-w-7xl grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 [&:last-child]:mb-0 [&+.cu-column]:mt-8 ">
+				<NumberCard />
+				<NumberCard />
+				<NumberCard />
+				<NumberCard />
 				<NumberCard />
 				<NumberCard />
 				<NumberCard />
@@ -173,17 +258,22 @@ export default function ContentEvents() {
 				quis, porta vestibulum erat.
 			</p>
 
+			<h3>4 col center aligned</h3>
+
 			<p>
-				Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-				risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-				Id dolor praesent donec est. Odio penatibus risus viverra tellus
-				varius sit neque erat velit. Faucibus commodo massa rhoncus,
-				volutpat. Dignissim sed eget risus enim. Mattis mauris semper
-				sed amet vitae sed turpis id. Id dolor praesent donec est. Odio
-				penatibus risus viverra tellus varius sit neque erat velit.
+				Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>
+				. Amet, massa quam varius orci dapibus volutpat cras. In amet eu
+				ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
+				viverra ridiculus non molestie. Gravida quis fringilla amet eget
+				dui tempor dignissim. Facilisis auctor venenatis varius nunc,
+				congue erat ac. Cras fermentum convallis quam.
 			</p>
 
 			<div className="cu-column mx-auto grid max-w-7xl grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 [&:last-child]:mb-0 [&+.cu-column]:mt-8 ">
+				<NumberCardCenter />
+				<NumberCardCenter />
+				<NumberCardCenter />
+				<NumberCardCenter />
 				<NumberCardCenter />
 				<NumberCardCenter />
 				<NumberCardCenter />
--- pages/cutheme/_components/Numbers/NumberCardCenter.tsx
+++ pages/cutheme/_components/Numbers/NumberCardCenter.tsx
@@ -1,13 +1,26 @@
-const styles = {
-	dt: `text-sm font-light text-cu-black-600 md:text-base`,
-	dd: `text-3xl font-semibold text-cu-black-800 md:text-4xl`,
-};
-
 export default function NumberCardCenter() {
+	let twColors = [
+		"border-l-slate-500",
+		"border-l-red-500",
+		"border-l-orange-300",
+		"border-l-amber-300",
+		"border-l-lime-400",
+		"border-l-teal-400",
+		"border-l-cyan-400",
+		"border-l-blue-400",
+	];
+	let color = twColors[Math.floor(Math.random() * twColors.length)];
+
 	return (
-		<div className="relative flex flex-col gap-2 px-6 py-5 text-center border rounded-lg shadow-lg border-cu-black-100">
-			<dt className={styles.dt}>Carleton Alumni</dt>
-			<dd className={styles.dd}>177,000+</dd>
+		<div
+			className={`not-prose relative flex flex-col gap-2 rounded-lg border border-l-8 border-cu-black-100 px-6 py-5 text-center shadow-lg md:max-w-md ${color}`}
+		>
+			<h3 className="text-sm font-light text-cu-black-600 md:text-base">
+				Carleton Alumni
+			</h3>
+			<p className="text-3xl font-semibold text-cu-black-800 md:text-4xl">
+				177,000+
+			</p>
 		</div>
 	);
 }
