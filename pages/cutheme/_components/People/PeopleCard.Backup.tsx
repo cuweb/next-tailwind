deae612413dfb3ce2c7cce978bca7428fea9727c
Troy Chaplin <troy.chaplin@gmail.com>
2023-01-26 10:31:07 -0500

    Finished people card updates

---
+++ pages/cutheme/_components/People/PeopleCard.Backup.tsx
@@ -0,0 +1,53 @@
+import { HeroIcon } from "rds-tailwind";
+
+export default function PeopleCardBackup() {
+	return (
+		<div className="relative hidden overflow-hidden duration-300 ease-in bg-white rounded-lg shadow-lg not-prose group hover:scale-105 md:max-w-lg">
+			<a href="https://carleton.ca" className="cursor-pointer">
+				<img
+					className="object-cover w-40 h-40 mx-auto my-8 border-4 border-white rounded-full shadow-lg"
+					src="https://static.wikia.nocookie.net/freshprince/images/a/ac/1carlton.jpg"
+					alt=""
+				/>
+				<div className="flex flex-col gap-1 mb-8 text-center">
+					<h3 className="text-xl font-semibold duration-300 ease-in text-cu-black group-hover:text-cu-red">
+						Carlton Banks
+					</h3>
+					<p className="text-base text-cu-black-600">
+						Manager, Peacock Stop
+					</p>
+				</div>
+				<ul className="flex justify-around text-base text-center border-t divide-x divide-cu-black-100 border-cu-black-100">
+					<li className="flex-auto">
+						<a
+							href="mailto:webdevs@carleton.ca"
+							className="flex items-center justify-center gap-2 p-3 font-medium text-cyan-700 hover:bg-cu-red hover:text-white"
+						>
+							<HeroIcon
+								aria-hidden
+								icon="InboxArrowDownIcon"
+								size="5"
+								isOutline
+							/>
+							Email
+						</a>
+					</li>
+					<li className="flex-auto">
+						<a
+							href="tel:6135202600"
+							className="flex items-center justify-center gap-2 p-3 font-medium text-cyan-700 hover:bg-cu-red hover:text-white"
+						>
+							<HeroIcon
+								aria-hidden
+								icon="DevicePhoneMobileIcon"
+								size="5"
+								isOutline
+							/>
+							Call
+						</a>
+					</li>
+				</ul>
+			</a>
+		</div>
+	);
+}
--- pages/cutheme/_components/People/PeopleCard.tsx
+++ pages/cutheme/_components/People/PeopleCard.tsx
@@ -1,52 +1,54 @@
-import { HeroIcon } from "rds-tailwind";
+const styles = {
+	listGroup: ``,
+	listItem: `flex text-white`,
+	itemLink: `text-xs text-cu-black-300 hover:text-white`,
+};
 
 export default function PeopleCard() {
 	return (
-		<div className="relative overflow-hidden duration-300 ease-in bg-white rounded-lg shadow-lg not-prose group hover:scale-105 md:max-w-lg">
-			<a href="https://carleton.ca" className="cursor-pointer">
+		<div className="not-prose group relative flex flex-col overflow-hidden duration-300 ease-in @container hover:scale-105 md:max-w-lg">
+			<a
+				href="https://carleton.ca"
+				className="relative flex flex-col h-full cursor-pointer"
+			>
 				<img
-					className="object-cover w-40 h-40 mx-auto my-8 border-4 border-white rounded-full shadow-lg"
+					className="object-cover mx-auto border-4 border-white rounded-full h-60 w-60 group-hover:shadow-md group-hover:duration-300 group-hover:ease-in"
 					src="https://static.wikia.nocookie.net/freshprince/images/a/ac/1carlton.jpg"
 					alt=""
 				/>
-				<div className="flex flex-col gap-1 mb-8 text-center">
-					<h3 className="text-xl font-semibold duration-300 ease-in text-cu-black group-hover:text-cu-red">
+
+				<div className="flex flex-col items-center py-6 space-y-2 px-7">
+					<h3 className="text-lg font-semibold text-cu-black duration-300 ease-in group-hover:text-cu-red @sm:md:text-2xl">
 						Carlton Banks
 					</h3>
-					<p className="text-base text-cu-black-600">
+					<p className="flex text-base text-cu-black-600 @sm:md:text-lg">
 						Manager, Peacock Stop
 					</p>
+					<ul className="pt-2 space-y-2 text-center">
+						<li className="text-sm  @sm:md:text-base">
+							<a
+								href="mailto:webdevs@carleton.ca"
+								className="font-semibold text-cyan-700 hover:text-cu-red"
+							>
+								george.hadjisophocleous@carleton.ca
+							</a>
+						</li>
+						<li className="items-center text-sm text-cu-black-600 hover:text-cu-red @sm:md:text-base">
+							<a href="#" className="">
+								613-520-2600 x1234
+							</a>
+						</li>
+					</ul>
 				</div>
-				<ul className="flex justify-around text-base text-center border-t divide-x divide-cu-black-100 border-cu-black-100">
-					<li className="flex-auto">
-						<a
-							href="mailto:webdevs@carleton.ca"
-							className="flex items-center justify-center gap-2 p-3 font-medium text-cyan-700 hover:bg-cu-red hover:text-white"
-						>
-							<HeroIcon
-								aria-hidden
-								icon="InboxArrowDownIcon"
-								size="5"
-								isOutline
-							/>
-							Email
-						</a>
-					</li>
-					<li className="flex-auto">
-						<a
-							href="tel:6135202600"
-							className="flex items-center justify-center gap-2 p-3 font-medium text-cyan-700 hover:bg-cu-red hover:text-white"
-						>
-							<HeroIcon
-								aria-hidden
-								icon="DevicePhoneMobileIcon"
-								size="5"
-								isOutline
-							/>
-							Call
-						</a>
-					</li>
-				</ul>
+
+				<div className="pb-5 mx-auto mt-auto px-7">
+					<p className="inline-flex px-2 mr-2 text-xs font-semibold rounded-xl bg-cu-black-50 text-cu-black-900 last:mr-0">
+						Tom Jones Love
+					</p>
+					<p className="inline-flex px-2 mr-2 text-xs font-semibold rounded-xl bg-cu-black-50 text-cu-black-900 last:mr-0">
+						Dance Superstar
+					</p>
+				</div>
 			</a>
 		</div>
 	);
