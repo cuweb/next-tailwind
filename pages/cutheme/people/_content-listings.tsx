70919ab49edb9c24cd764c112da6137d6cf18e48
Troy Chaplin <troy.chaplin@gmail.com>
2023-01-27 21:54:00 -0500

    Updated people list item

--- pages/cutheme/people/_content-listings.tsx
+++ pages/cutheme/people/_content-listings.tsx
@@ -1,4 +1,4 @@
-import { Container } from "rds-tailwind";
+import { Column, Container } from "rds-tailwind";
 import PeopleListItem from "../_components/People/PeopleListItem";
 
 const styles = {
@@ -9,106 +9,16 @@ const styles = {
 export default function PeopleListingContent() {
 	return (
 		<>
-			<h2>H2 Heading</h2>
-			<p>
-				Quis semper vulputate aliquam venenatis egestas sagittis quisque
-				orci. Donec commodo sit viverra aliquam porttitor ultrices
-				gravida eu. Tincidunt leo, elementum mattis elementum ut nisl,
-				justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis.
-				Amet, duis sed elit interdum dignissim.
-			</p>
-			<div className="cu-panel not-prose overflow-hidden rounded-lg border border-cu-black-100 bg-white shadow-lg [&+.cu-panel]:mt-8">
-				<div
-					className={`grid overflow-hidden md:grid-cols-1 ${styles.oneColBorder}`}
-				>
-					<PeopleListItem />
-				</div>
-			</div>
-			<p>
-				Quis semper vulputate aliquam venenatis egestas sagittis quisque
-				orci. Donec commodo sit viverra aliquam porttitor ultrices
-				gravida eu. Tincidunt leo, elementum mattis elementum ut nisl,
-				justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis.
-				Amet, duis sed elit interdum dignissim.
-			</p>
-			<p>
-				Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-				risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-				Id dolor praesent donec est. Odio penatibus risus viverra tellus
-				varius sit neque erat velit. Phasellus et lectus elementum,
-				dictum odio quis, euismod augue. Nulla ultricies orci eu
-				volutpat mollis. Aenean mauris leo, congue id laoreet vitae,
-				bibendum at dui. Proin ac aliquet enim, a ullamcorper tortor. Ut
-				mattis, ante id porttitor elementum, lorem mauris rutrum enim,
-				ut tristique est est at turpis. Nam hendrerit luctus ex, ac
-				rhoncus mauris pretium varius. Phasellus dignissim sodales
-				feugiat.
-			</p>
-			<div className="cu-panel not-prose overflow-hidden rounded-lg border border-cu-black-100 bg-white shadow-lg [&+.cu-panel]:mt-8">
-				<div
-					className={`grid overflow-hidden md:grid-cols-1 ${styles.oneColBorder}`}
-				>
-					<PeopleListItem />
-					<PeopleListItem />
-					<PeopleListItem />
-				</div>
-			</div>
+			<Container>
+				<Column cols="1">
+					<div className="cu-panel not-prose overflow-hidden rounded-lg border border-cu-black-100 bg-white shadow-lg [&+.cu-panel]:mt-8">
+						<div
+							className={`grid overflow-hidden md:grid-cols-1 ${styles.oneColBorder}`}
+						>
+							<PeopleListItem />
+						</div>
+					</div>
 
-			{/* <p>
-				Quis semper vulputate aliquam venenatis egestas sagittis quisque
-				orci. Donec commodo sit viverra aliquam porttitor ultrices
-				gravida eu. Tincidunt leo, elementum mattis elementum ut nisl,
-				justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis.
-				Amet, duis sed elit interdum dignissim.
-			</p>
-			<p>
-				Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-				risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-				Id dolor praesent donec est. Odio penatibus risus viverra tellus
-				varius sit neque erat velit. Phasellus et lectus elementum,
-				dictum odio quis, euismod augue. Nulla ultricies orci eu
-				volutpat mollis. Aenean mauris leo, congue id laoreet vitae,
-				bibendum at dui. Proin ac aliquet enim, a ullamcorper tortor. Ut
-				mattis, ante id porttitor elementum, lorem mauris rutrum enim,
-				ut tristique est est at turpis. Nam hendrerit luctus ex, ac
-				rhoncus mauris pretium varius. Phasellus dignissim sodales
-				feugiat.
-			</p> */}
-
-			<div className="cu-panel not-prose overflow-hidden rounded-lg border border-cu-black-100  bg-white shadow-lg [&+.cu-panel]:mt-8">
-				<div
-					className={`grid overflow-hidden md:grid-cols-2 ${styles.twoColBorder}`}
-				>
-					<PeopleListItem />
-					<PeopleListItem />
-					<PeopleListItem />
-					<PeopleListItem />
-				</div>
-			</div>
-			<p>
-				Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-				risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-				Id dolor praesent donec est. Odio penatibus risus viverra tellus
-				varius sit neque erat velit. Phasellus et lectus elementum,
-				dictum odio quis, euismod augue. Nulla ultricies orci eu
-				volutpat mollis. Aenean mauris leo, congue id laoreet vitae,
-				bibendum at dui. Proin ac aliquet enim, a ullamcorper tortor. Ut
-				mattis, ante id porttitor elementum, lorem mauris rutrum enim,
-				ut tristique est est at turpis. Nam hendrerit luctus ex, ac
-				rhoncus mauris pretium varius. Phasellus dignissim sodales
-				feugiat.
-			</p>
-
-			<p>
-				Quis semper vulputate aliquam venenatis egestas sagittis quisque
-				orci. Donec commodo sit viverra aliquam porttitor ultrices
-				gravida eu. Tincidunt leo, elementum mattis elementum ut nisl,
-				justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis.
-				Amet, duis sed elit interdum dignissim.
-			</p>
-
-			<Container bgColor="grey">
-				<div className="max-w-5xl mx-auto">
 					<div className="cu-panel not-prose overflow-hidden rounded-lg border border-cu-black-100 bg-white shadow-lg [&+.cu-panel]:mt-8">
 						<div
 							className={`grid overflow-hidden md:grid-cols-1 ${styles.oneColBorder}`}
@@ -118,32 +28,7 @@ export default function PeopleListingContent() {
 							<PeopleListItem />
 						</div>
 					</div>
-				</div>
-			</Container>
 
-			<p>
-				Quis semper vulputate aliquam venenatis egestas sagittis quisque
-				orci. Donec commodo sit viverra aliquam porttitor ultrices
-				gravida eu. Tincidunt leo, elementum mattis elementum ut nisl,
-				justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis.
-				Amet, duis sed elit interdum dignissim.
-			</p>
-			<p>
-				Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-				risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-				Id dolor praesent donec est. Odio penatibus risus viverra tellus
-				varius sit neque erat velit. Phasellus et lectus elementum,
-				dictum odio quis, euismod augue. Nulla ultricies orci eu
-				volutpat mollis. Aenean mauris leo, congue id laoreet vitae,
-				bibendum at dui. Proin ac aliquet enim, a ullamcorper tortor. Ut
-				mattis, ante id porttitor elementum, lorem mauris rutrum enim,
-				ut tristique est est at turpis. Nam hendrerit luctus ex, ac
-				rhoncus mauris pretium varius. Phasellus dignissim sodales
-				feugiat.
-			</p>
-
-			<Container bgColor="grey">
-				<div className="max-w-5xl mx-auto">
 					<div className="cu-panel not-prose overflow-hidden rounded-lg border border-cu-black-100  bg-white shadow-lg [&+.cu-panel]:mt-8">
 						<div
 							className={`grid overflow-hidden md:grid-cols-2 ${styles.twoColBorder}`}
@@ -154,29 +39,41 @@ export default function PeopleListingContent() {
 							<PeopleListItem />
 						</div>
 					</div>
-				</div>
+				</Column>
 			</Container>
 
-			<p>
-				Quis semper vulputate aliquam venenatis egestas sagittis quisque
-				orci. Donec commodo sit viverra aliquam porttitor ultrices
-				gravida eu. Tincidunt leo, elementum mattis elementum ut nisl,
-				justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis.
-				Amet, duis sed elit interdum dignissim.
-			</p>
-			<p>
-				Quis semper vulputate aliquam venenatis egestas sagittis quisque
-				orci. Donec commodo sit viverra aliquam porttitor ultrices
-				gravida eu. Tincidunt leo, elementum mattis elementum ut nisl,
-				justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis.
-				Amet, duis sed elit interdum dignissim.
-			</p>
-			<p>
-				Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-				risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-				Id dolor praesent donec est. Odio penatibus risus viverra tellus
-				varius sit neque erat velit.
-			</p>
+			<Container bgColor="grey">
+				<Column cols="1">
+					<div className="cu-panel not-prose overflow-hidden rounded-lg border border-cu-black-100 bg-white shadow-lg [&+.cu-panel]:mt-8">
+						<div
+							className={`grid overflow-hidden md:grid-cols-1 ${styles.oneColBorder}`}
+						>
+							<PeopleListItem />
+						</div>
+					</div>
+
+					<div className="cu-panel not-prose overflow-hidden rounded-lg border border-cu-black-100 bg-white shadow-lg [&+.cu-panel]:mt-8">
+						<div
+							className={`grid overflow-hidden md:grid-cols-1 ${styles.oneColBorder}`}
+						>
+							<PeopleListItem />
+							<PeopleListItem />
+							<PeopleListItem />
+						</div>
+					</div>
+
+					<div className="cu-panel not-prose overflow-hidden rounded-lg border border-cu-black-100  bg-white shadow-lg [&+.cu-panel]:mt-8">
+						<div
+							className={`grid overflow-hidden md:grid-cols-2 ${styles.twoColBorder}`}
+						>
+							<PeopleListItem />
+							<PeopleListItem />
+							<PeopleListItem />
+							<PeopleListItem />
+						</div>
+					</div>
+				</Column>
+			</Container>
 		</>
 	);
 }
